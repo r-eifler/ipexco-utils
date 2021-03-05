8072eaef7ed0b50aec002143bf1d451ecd9a6eb4
Rebecca Eifler <eifler@cs.uni-saarland.de>
2021-03-05 16:09:05 +0100

    cleanup settings

--- demo_generator/settings.py
+++ demo_generator/settings.py
@@ -4,10 +4,8 @@ args_optimal_call = ['--search', '\"astar(lmcut())\"']
 args_mugs_call = ['--heuristic', 'h=\"hc(nogoods=false, cache_estimates=false)\"', '--heuristic',
    'mugs_h=\"mugs_hc(hc=h, all_softgoals=false)\"', '--search', '\"dfs(u_eval=mugs_h)\"']
 
-#args_mugs_call = ["--heuristic", "hp=\"iPDB()\"",
- #   "--search", "\"goal_relation([lazy_greedy([hp], reopen_closed=true)], heu=[hp], metasearch=bottomupmugssearch)\""]
-
 # --heuristic h="hc(nogoods=false, cache_estimates=false)" --heuristic mugs_h="mugs_hc(hc=h, all_softgoals=false)" --search "dfs(u_eval=mugs_h)"
+
 
 # FD = "/home/eifler/XPP/framework/fast-downward/run_FD.py"
 # FD_script_name = "run_FD.py"
