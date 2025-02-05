183e27778ef46480f989f126576634e8ba5b1ec7
Rebecca Eifler <eifler@cs.uni-saarland.de>
2025-02-05 19:53:29 +0100

    remove outdates files and add README

---
+++ README.md
@@ -0,0 +1,4 @@
+# IPEXCO Utils
+
+This is a collection of python scripts used by the iterative planning toll 
+[IPEXCO](https://github.com/r-eifler/IPEXCO-frontend).
--- demo_generator/hard_goals.py
+++
@@ -1,7 +0,0 @@
-import json
-
-def parseHardGoals(path):
-    file = open(path)
-    jsonObject = json.load(file)
-    file.close()
-    return jsonObject['hard_goals']
